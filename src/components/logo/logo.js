597e525c0ddfc6cea4bf22affb33891a3a7cb8b1
Peter Rawlings <peterawlings24@hotmail.co.uk>
2021-03-29 18:07:02 -0400

    Formatting logo files code

--- src/components/logo/logo.js
+++ src/components/logo/logo.js
@@ -6,9 +6,11 @@ import './logo.css';
 const Logo = () => {
     return (
         <div className='ma4 mt0'>
-        <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
-            <div className="Tilt-inner pa4"><img src={Face}></img></div>
-        </Tilt>
+            <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }}>
+                <div className="Tilt-inner pa4">
+                    <img src={Face} alt='logo'></img>
+                </div>
+            </Tilt>
         </div>
     );
 }
