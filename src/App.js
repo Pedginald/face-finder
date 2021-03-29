d000339d31e7785727e56645d025ba13cc3b6bf5
Peter Rawlings <peterawlings24@hotmail.co.uk>
2021-03-29 18:06:22 -0400

    Creating a form for the image link

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Navigation from './components/navigation/navigation';
 import Logo from './components/logo/logo.js';
+import ImageLinkForm from './components/imageLinkForm/imageLinkForm.js';
 import './App.css';
 
 class App extends Component {
@@ -9,8 +10,8 @@ class App extends Component {
       <div className="App">
         <Navigation />
         {<Logo />
-        /*<ImageLinkForm />
-        <FaceRecognition />*/}
+        <ImageLinkForm />
+        /*<FaceRecognition />*/}
       </div>
     );
   }
