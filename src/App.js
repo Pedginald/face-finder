20faacfa81cb7ab32fde42efa076d2786a7a1158
Peter Rawlings <peterawlings24@hotmail.co.uk>
2021-03-30 16:14:35 -0400

    Altering the import syntax of rank component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Navigation from './components/navigation/navigation';
 import Logo from './components/logo/logo';
 import ImageLinkForm from './components/imageLinkForm/imageLinkForm';
-import Rank from './components/rank/rank.js'
+import Rank from './components/rank/rank'
 import Particles from 'react-particles-js';
 import './App.css';
 
