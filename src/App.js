02dd2ae1ed9c5e0cde545b8b5e7d85f3f2b0ffd8
Peter Rawlings <peterawlings24@hotmail.co.uk>
2021-03-30 13:00:50 -0400

    Adding properties to particle component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
 import React, { Component } from 'react';
 import Navigation from './components/navigation/navigation';
-import Logo from './components/logo/logo.js';
+import Logo from './components/logo/logo';
 import ImageLinkForm from './components/imageLinkForm/imageLinkForm';
+import Rank from './components/rank/rank.js'
+import Particles from 'react-particles-js';
 import './App.css';
 
+const particlesOptions = {
+    particles: {
+        number: {
+            value: 30,
+            density: {
+                enable: true,
+                value_area: 800
+            }
+        }
+    }
+}
+
 class App extends Component {
-  render () {
-    return (
-      <div className="App">
-        <Navigation />
-        <Logo />
-        <ImageLinkForm />
-        {/*<FaceRecognition />*/}
-      </div>
-    );
-  }
+    render () {
+        return (
+            <div className="App">
+                <Particles className='particles'
+                    params={{particlesOptions}}
+                />
+                <Navigation />
+                <Logo />
+                <Rank />
+                <ImageLinkForm />
+                {/*<FaceRecognition />*/}
+            </div>
+        );
+    }
 }
 
 export default App;
